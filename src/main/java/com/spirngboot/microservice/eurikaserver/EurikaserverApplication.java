e14e5f25f1e8eae3333e6c81717f0b23e2f2001b
Andrei <109a@mail.ru>
2021-04-21 17:36:00 +0300

    First commit

---
+++ src/main/java/com/spirngboot/microservice/eurikaserver/EurikaserverApplication.java
@@ -0,0 +1,16 @@
+package com.spirngboot.microservice.eurikaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurikaserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurikaserverApplication.class, args);
+    }
+
+}
